952339349da8f08df0ef26517e68474cf47ef166
Yash <100015221+Yash0770@users.noreply.github.com>
2023-04-11 17:32:24 +0530

    Add files via upload

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -5,44 +5,68 @@ import React, { useEffect, useState } from 'react'
 function Main() {
     const [input, setInput] = useState('');
     
-    // const handleClick = (event) => {
-    //     const value = event.target.value;
-    //     setInput((input + value).slice(0,2));
-    // }
-
     const handleClick = (event) => {
-        const value = event.target.value;
-        const allowedOperations = ['+', '-', '*', '/'];
+    const value = event.target.value;
+    const allowedOperations = ['+', '-', '*', '/'];
     
-        // check if the input is an arithmetic operation
-        if (allowedOperations.includes(value)) {
-            // check if an arithmetic operation already exists
-            if (input.includes('+') || input.includes('-') || input.includes('*') || input.includes('/')) {
-                return;
-            }
-        }
-    
-        // check if the input is a number
-        if (!isNaN(value)) {
-            // check if the input length is less than or equal to 14
-            if (input.length < 14) {
-                setInput(input + value);
-            }
-        } else {
-            // the input is an arithmetic operation
-            setInput(input + value);
+    // check if the input is an arithmetic operation
+    if (allowedOperations.includes(value)) {
+        // check if an arithmetic operation already exists
+        if (input.includes('+') || input.includes('-') || input.includes('*') || input.includes('/')) {
+            // setInput(input.slice(0,-1) + value)
+            setInput(input.slice(0,-13) + value)
+            return;
         }
     }
+    // check if the input is a number
+    if (!isNaN(value)) {
+        // check if the input length is less than or equal to 11
+        if (input.length < 11) {
+            setInput(input + value);
+        }
+    } else {
+        // the input is an arithmetic operation
+        setInput(input + value);
+        }
+    }
+
+    const handleKeyDown = (event) => {
+        const key = event.key;
+        const allowedKeys = ['Backspace', 'Enter', '+', '-', '*', '/'];
     
-    useEffect(()=>{
+        // check if the pressed key is a number or an arithmetic operator
+        if (!isNaN(key) || allowedKeys.includes(key)) {
+          // check if an arithmetic operation already exists
+          if (
+            allowedKeys.includes(key) &&
+            (input.includes('+') ||
+              input.includes('-') ||
+              input.includes('*') ||
+              input.includes('/'))
+          ) {
+            event.preventDefault();
+            setInput(input.slice(0, -13) + key);
+          }
+        } else {
+          event.preventDefault();
+        }
+    
+        // perform actions for Backspace and Enter keys
+        if (key === 'Backspace') {
+          setInput(input.slice(0, -1));
+        } else if (key === 'Enter') {
+          calculate();
+        }
+      };
+    // useEffect(()=>{
         
-    },[input])
+    // },[input])
     const calculate = () => {
         try {
         const result = eval(input);
-        setInput('=' + result.toString());
+        setInput(result.toString());
         } catch (error) {
-            // setInput('Error');
+            setInput('Error');
             if(error){
                 setInput('Not Valid!')
             }
@@ -54,14 +78,12 @@ function Main() {
     }
 
   return (
-    <div className='container '>
+    <div className='container'>
         <br /><br />
         <center>
-        <section className="heading mb-4"><span> Calculator</span></section>
-            {/* <label htmlFor="">Hey</label> */}
-        {/* <form action='' className='form-control formClass'> */}
+        <section className="heading mb-4"><span><h1>Calculator</h1></span></section>
         <div className='form-control formClass'>
-        <input className='mt-4 inputClass' maxLength={'2'} type="text" value={input} style={{textAlign:'right'}} /><br />
+        <input className='mt-4 inputClass' type="text" onKeyDown={handleKeyDown} value={input} style={{textAlign:'right'}} /><br />
 
          <br />
         <div className="container d-flex justify-content-center ">
@@ -91,8 +113,6 @@ function Main() {
              <button className='btn btn-primary arithmeticBtn me-2' onClick={calculate}>=</button>&nbsp;&nbsp;
              <button className='btn btn-primary arithmeticBtn ' value='/' onClick={handleClick} >/</button>
          </div>
-             {/* <button value='C' onClick={handleClick}>Clear</button> */}
-             {/* </form> */}
              </div> 
              </center>
              
